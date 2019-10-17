028f0a9428059e32e61ed151ec12cde91a9c8e75
Carhuacusma <51172263+Carhuacusma@users.noreply.github.com>
2019-10-17 13:55:55 -0500

    Auxiliar del input a mano
    TO DO: hashing para identificar modelos de cajas

---
+++ aux_TF_input.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#input
+
+def defContenedor():
+    X = int(input())
+    Y = int(input())
+    Z = int(input())
+    return (X,Y,Z)
+
+def defCajas():
+    # TO DO
+    nTipos = int(input())
+    contT = 0
+    arrCajas = []
+    def hashing():
+        if nTipos > 26:
+            return 
+        return contT + 64
+    for i in range(n):
+        X = int(input())
+        Y = int(input())
+        Z = int(input())
+    
+
+defContenedor()
+
+
+# In[ ]:
+
+
+print()
+
