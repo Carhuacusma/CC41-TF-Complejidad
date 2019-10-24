aa4125c2e5a43fed0f2d900177dea34541b65122
iller15 <39936760+iller15@users.noreply.github.com>
2019-10-23 21:27:15 -0500

    Agregado Función Display3D

--- Lectura y Escribir Datos.py
+++ Lectura y Escribir Datos.py
@@ -1,3 +1,8 @@
+import random 
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
 class Paralelepipedo:
     def __init__(self,largo, ancho, alto, Myid):
         self.largo = largo
@@ -19,21 +24,21 @@ a = [1,1,1]
 MyRec = []
 nPLLPPD = []
 cont = 0
-Contenedores = []
+Contenedores = []#arreglo de arrglo de alto,ancho,alto,volumen
 
 def leerTxD(cont):
     archivo = open("Entrada.txt","r")
-    n = archivo.readlines()
+    n = archivo.readlines() 
     for i in n:
         contenedor = i
         helpm = contenedor.split(" ")
         if cont == 0:
-            a[0] = int(helpm[0])
-            a[1] = int(helpm[1])
-            a[2] = int(helpm[2])
+            la = int(helpm[0])
+            an = int(helpm[1])
+            al = int(helpm[2])
             
-            volumen = a[0] * a[1] * a[2]
-            Contenedores.append([a[0],a[1], a[2], volumen])
+            volumen = la * an * al
+            Contenedores.append([la,an, al, volumen])
 
 
         elif cont == 1:
@@ -46,26 +51,33 @@ def leerTxD(cont):
                     MyRec[cont-2].append(Paralelepipedo(int(helpm[2]),int(helpm[3]),int(helpm[4]),helpm[1]+str(x+1)))
     
         cont +=1
+    for i in range(len(MyRec)):
+        for z in range(len(MyRec[i])):
+            MyRec[i][z].Pos = (random.randint(0,20),random.randint(0,20),random.randint(0,20))
         
-        
+"""        
 leerTxD(cont)
 print(a)
 print(nPLLPPD)
 #print(contenedor_Ancho,contenedor_Largo,contenedor_Alto)
 for x in range(len(MyRec)):
     for i in range(len(MyRec[x])):
-        MyRec[x][i].display()
+        MyRec[x][i].display()"""
 
 def Guardar():
     archivo = open("Archivo Salida", 'w+')
     n = len(MyRec) #Numero de cajas a transportar
     p = len(Contenedores) #Numero de contenedores usados
     vu = 0 #Volumen utilizado
-    vd = 0 #Volumen disponible
+    vd = Contenedores[0][3]*p #Volumen disponible
     
+    for i in range(len(MyRec)):
+        for x in range(len(MyRec[i])):
+            vu += (MyRec[i][x].alto*MyRec[i][x].largo*MyRec[i][x].ancho)
+        
     a = "Contenedores usados: " + str(p) + "\n" 
     b = "Volumen disponible: " + str(vd) + " m2 \n"
-    c = "Volumen ocupado: " + str(vu) + " m2 \n"
+    c = "Volumen ocupado: " + str(vu)  + " m2 (" + str(vu*100/vd) + "%) \n"
     d = "Cajas a transportar: " + str(n) + "\n"
     
     archivo.write(a+b+c+d)
@@ -74,8 +86,38 @@ def Guardar():
     for i in range(n):
         for z in range(len(MyRec[i])):
             archivo.write("%d\t\t\t%s\t\t\t%d%d\t\t\t%d\n"%(MyRec[i][z].Contenedor,MyRec[i][z].Myid,MyRec[i][z].Pos[0],MyRec[i][z].Pos[1],MyRec[i][z].rotacion))
-            print("salio")
+            
+            
+def Display3D():
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111, projection = '3d')
+    
+    xpos = []
+    ypos = []
+    zpos = []
+    
+    dx = []
+    dy = []
+    dz = []
+    
+    for i in range(len(MyRec)):
+        for y in range(len(MyRec[i])):
+            xpos.append(MyRec[i][y].Pos[0])
+            ypos.append(MyRec[i][y].Pos[1])
+            zpos.append(MyRec[i][y].Pos[2])
+            
+            dx.append(MyRec[i][y].largo)
+            dy.append(MyRec[i][y].ancho)
+            dz.append(MyRec[i][y].alto)
         
+    
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_zlabel('Z')
+    
+    ax1.bar3d(xpos, ypos, zpos, dx, dy, dz, color = '#00ceaa')
+    plt.show()
+leerTxD(cont)
 Guardar()
-
-
+Display3D()
+            
