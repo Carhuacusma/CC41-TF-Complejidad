e93d2b7e3f559588cd86de2e0081a92969b79276
Carhuacusma <51172263+Carhuacusma@users.noreply.github.com>
2019-11-12 21:23:56 -0500

    Add files via upload

--- TF_Algoritmo Dante.py
+++ TF_Algoritmo Dante.py
@@ -4,11 +4,11 @@
 # In[3]:
 
 
-# arreglo = [ (ind,rot), 
-#             (ind,rot), ... ]
+# arreglo = [ ind,
+#             ind ... ]
 # indices = [ ['A', (size.x,size.y,size.z)], 
 #             ['B', (size.x,size.y,size.z)], ...]
-def kindaTetris(arreglo, indices,):
+def kindaTetris(arreglo, indices, contenedor):
     n = len(arreglo)
     #        max med min
     # rot 1:  X   Y   Z
@@ -17,13 +17,19 @@ def kindaTetris(arreglo, indices,):
     # rot 4:  Y   Z   X
     # rot 5:  Z   X   Y
     # rot 6:  Z   Y   X
-    def ordenarDentroContenedor(arr):
+    def dentroContenedor(arr):
         nA = len(arr)
         waste = [[None]*6 for _ in range(nA)]
         # waste[rec i][orientacion] = menor desperdicio al guardar 
         #                             todas las cajas hasta la caja i
         #                             ( de arr )
         resp = []
+        def aux(i,j): # caja i, en la orientacion j
+            if i == 0:
+                ind, size = indices[arr[i]]
+                vol = size[0]*size[1]*size[2]
+                for col in range(6):
+                    waste[i][j] = (contenedor[0]*contenedor[1]*contenedor[2]) - vol
 
 
 # In[ ]:
