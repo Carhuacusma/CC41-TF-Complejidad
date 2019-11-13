41777a14573f9d8bec607a2d67f9df50e0bf0b08
Carhuacusma <51172263+Carhuacusma@users.noreply.github.com>
2019-11-12 21:51:15 -0500

    Planteamiento de tablas de desperdicio

--- TF_Algoritmo Dante.py
+++ TF_Algoritmo Dante.py
@@ -23,13 +23,26 @@ def kindaTetris(arreglo, indices, contenedor):
         # waste[rec i][orientacion] = menor desperdicio al guardar 
         #                             todas las cajas hasta la caja i
         #                             ( de arr )
-        resp = []
+        dentro = []
+        def entra(caja):
+            # caja = [ (pos.x, pos.y, pos.z) ,(size.x, size.y, size.z)]
+            pos, size = caja
+            for c in dentro:
+                if pos[0] + size[0]
+                # comparar pos + size si entra dentro del contenedor (arreglo dentro)
         def aux(i,j): # caja i, en la orientacion j
+            # el objetivo es que busque ordenar la nueva caja
+            # en la opcion donde el desperdicio es mayor
+            # waste[i][j] = (Max x,Max y, Max z)
+            if waste[i][j] != None:
+                return
             if i == 0:
                 ind, size = indices[arr[i]]
                 vol = size[0]*size[1]*size[2]
                 for col in range(6):
                     waste[i][j] = (contenedor[0]*contenedor[1]*contenedor[2]) - vol
+            else:
+                
 
 
 # In[ ]:
